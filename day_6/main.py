aa14c6fc3c285ca8aa4fea13cba1fde2b9de5e81
ShadoWing1 <oyungemer14@gmail.com>
2022-12-06 17:08:48 +0300

    fix

---
+++ day_6/main.py
@@ -0,0 +1,12 @@
+def cozum1(dosya):
+    pass
+
+def cozum2(dosya):
+    pass
+
+with open("input.txt") as f:
+    dosya = f
+    print(f)
+
+print(f"cozum1: {cozum1(dosya)}")
+print(f"cozum2: {cozum2(dosya)}")
